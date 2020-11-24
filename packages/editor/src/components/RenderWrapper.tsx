bccca4d45f9dcf5a4b09bd3e913919354a65c20a
moonrailgun <moonrailgun@gmail.com>
2020-11-24 17:08:29 +0800

    refactor: use line to replace drop area

--- packages/editor/src/components/RenderWrapper.tsx
+++ packages/editor/src/components/RenderWrapper.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { useDrop } from 'react-dnd';
 import { TemplateItemSymbol } from '../symbol';
 import classNames from 'classnames';
-import type { ASTNode, ASTType } from '@saucer/core/lib/ast/types';
-import { DropArea } from './DropArea';
+import type { ASTNode } from '@saucer/core/lib/ast/types';
 import { DropIndicator } from './DropIndicator';
 import {
   findCup,
@@ -13,37 +12,118 @@ import {
 } from '@saucer/core';
 import type { DragObject } from '../types';
 
+function useDragAndDrop(props: RenderWrapperProps) {
+  const { path, tea } = props;
+  const teaType = tea.type;
+  const {
+    dispatchInsertBefore,
+    dispatchInsertAfter,
+    dispatchAppendChildren,
+  } = useASTDispatchAction();
+  const ref = useRef<HTMLDivElement>(null);
+  const [hoverDirection, setHoverDirection] = useState<'bottom' | 'top'>(
+    'bottom'
+  );
+
+  const handleDrop = useCallback(
+    (cupName: string) => {
+      const cup = findCup(cupName);
+      if (cup === null) {
+        console.error('Cannot find cup by name:' + cupName);
+        return;
+      }
+
+      if (teaType === 'container') {
+        dispatchAppendChildren(path, cup.type, cup.name);
+      } else {
+        if (hoverDirection === 'top') {
+          dispatchInsertBefore(path, cup.type, cup.name);
+        } else {
+          dispatchInsertAfter(path, cup.type, cup.name);
+        }
+      }
+    },
+    [
+      teaType,
+      path,
+      hoverDirection,
+      dispatchInsertBefore,
+      dispatchInsertAfter,
+      dispatchAppendChildren,
+    ]
+  );
+
+  const [{ isOverCurrent, dragName }, dropRef] = useDrop({
+    accept: [TemplateItemSymbol],
+    drop: (item: DragObject, monitor) => {
+      const didDrop = monitor.didDrop();
+      if (didDrop) {
+        // Skip if any one handle this event.
+        return;
+      }
+
+      handleDrop(item.name);
+    },
+    hover: (item, monitor) => {
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      if (!hoverBoundingRect) {
+        return;
+      }
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      if (clientOffset === null) {
+        return;
+      }
+
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      const direction = hoverClientY > hoverMiddleY ? 'bottom' : 'top';
+      setHoverDirection(direction);
+    },
+    collect: (monitor) => ({
+      isOverCurrent: monitor.isOver({ shallow: true }),
+      dragName: monitor.getItem()?.name,
+    }),
+  });
+
+  const dndRef = dropRef(ref);
+
+  const dndClassName = useMemo(() => {
+    if (!isOverCurrent) {
+      return '';
+    }
+
+    if (teaType !== 'leaf') {
+      return '';
+    }
+
+    return `saucer-render-wrapper__hover-${hoverDirection}`;
+  }, [isOverCurrent, hoverDirection]);
+
+  return { dndRef, isOverCurrent, dragName, dndClassName };
+}
+
 interface RenderWrapperProps {
   path: string;
   tea: ASTNode;
 }
 export const RenderWrapper: React.FC<RenderWrapperProps> = React.memo(
   (props) => {
-    const { path, tea } = props;
+    const { tea } = props;
     const type = tea.type;
-    const {
-      dispatchInsertAfter,
-      dispatchAppendChildren,
-    } = useASTDispatchAction();
     const currentSelectedTeaId = useCurrentTeaId();
     const isSelected = tea.id === currentSelectedTeaId;
     const setCurrentTea = useSetCurrentTea();
 
-    const handleDrop = useCallback(
-      (cupName: string) => {
-        const cup = findCup(cupName);
-        if (cup === null) {
-          console.error('Cannot find cup by name:' + cupName);
-          return;
-        }
-
-        if (type === 'container') {
-          dispatchAppendChildren(path, cup.type, cup.name);
-        } else {
-          dispatchInsertAfter(path, cup.type, cup.name);
-        }
-      },
-      [type, path, dispatchInsertAfter, dispatchAppendChildren]
+    const { dndRef, isOverCurrent, dragName, dndClassName } = useDragAndDrop(
+      props
     );
 
     const handleClick = useCallback(
@@ -54,22 +134,6 @@ export const RenderWrapper: React.FC<RenderWrapperProps> = React.memo(
       [setCurrentTea, tea.id]
     );
 
-    const [{ isOverCurrent, dragName }, dropRef] = useDrop({
-      accept: [TemplateItemSymbol],
-      drop: (item: DragObject, monitor) => {
-        const didDrop = monitor.didDrop();
-        if (didDrop) {
-          return;
-        }
-
-        handleDrop(item.name);
-      },
-      collect: (monitor) => ({
-        isOverCurrent: monitor.isOver({ shallow: true }),
-        dragName: monitor.getItem()?.name,
-      }),
-    });
-
     let el: React.ReactNode;
     if (type === 'container') {
       el = (
@@ -79,18 +143,13 @@ export const RenderWrapper: React.FC<RenderWrapperProps> = React.memo(
         </>
       );
     } else {
-      el = (
-        <>
-          <DropArea path={path} position="before" />
-          {props.children}
-        </>
-      );
+      el = <>{props.children}</>;
     }
 
     return (
       <div
-        ref={dropRef}
-        className={classNames('saucer-render-wrapper', {
+        ref={dndRef as any}
+        className={classNames(dndClassName, 'saucer-render-wrapper', {
           'saucer-render-wrapper__selected': isSelected,
           'saucer-render-wrapper__hover': isOverCurrent,
         })}
