248e4c6683131da26766bc3f1b045f2c61be04c3
moonrailgun <moonrailgun@gmail.com>
2020-12-28 22:17:57 +0800

    feat: add attr getter and setter for CSSEditor

--- packages/css-editor/src/CSSEditor.tsx
+++ packages/css-editor/src/CSSEditor.tsx
@@ -1,23 +1,46 @@
 import React, { useCallback, useState } from 'react';
 import EditorList, { EditorListChangeState } from 'rc-editor-list';
 import _get from 'lodash/get';
+import _isNil from 'lodash/isNil';
 import { parseStyleStringToReactStyle } from './utils';
+import { useTeaAttrsContext } from '@saucerjs/editor';
+import { useCurrentTeaId } from '@saucerjs/core';
 
+/**
+ * 样式编辑器
+ */
 export const CSSEditor: React.FC = React.memo(() => {
   const [dom, setDom] = useState<HTMLDivElement | null>(null);
+  const { currentTeaAttrs, setCurrentTeaAttrs } = useTeaAttrsContext();
+  const currentTeaId = useCurrentTeaId();
+  const styleAttr = currentTeaAttrs['style'] ?? {};
 
-  const handleChange = useCallback((state: EditorListChangeState) => {
-    const styleObj = parseStyleStringToReactStyle(
-      _get(state, ['cssValue', '', 'css', 'default'])
-    );
+  const handleChange = useCallback(
+    (state: EditorListChangeState) => {
+      const cssString = _get(state, ['cssValue', '', 'css', 'default']);
+      const styleObj = parseStyleStringToReactStyle(cssString);
 
-    // TODO: write to tea
-    console.log(styleObj);
-  }, []);
+      setCurrentTeaAttrs({
+        style: styleObj,
+      });
+    },
+    [setCurrentTeaAttrs]
+  );
+
+  const handleRef = (ref: HTMLDivElement) => {
+    if (_isNil(ref)) {
+      return;
+    }
+    for (const attr in styleAttr) {
+      const val = styleAttr[attr];
+      ref.style.setProperty(attr, val);
+    }
+    setDom(ref);
+  };
 
   return (
-    <div>
-      <div ref={setDom}></div>
+    <div key={currentTeaId}>
+      <div ref={handleRef}></div>
 
       {dom && (
         <EditorList
