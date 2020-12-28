50f922a770f7d9e595ff10d9a5f7db87737bbc01
moonrailgun <moonrailgun@gmail.com>
2020-12-28 09:53:38 +0800

    chore: script and path

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SaucerEditor } from '@saucerjs/editor';
-import '@saucerjs/editor/lib/default.css';
+import '@saucerjs/editor/assets/default.css';
 import '@saucerjs/css-editor/lib/style';
 
 export const App: React.FC = React.memo(() => {
