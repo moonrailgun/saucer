49caebad5c9e6e337aa326333c53a12130ce4c81
moonrailgun <moonrailgun@gmail.com>
2021-02-28 20:18:47 +0800

    refactor: export more

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,4 +1,7 @@
 export { Previewer } from './Previewer';
+export { TreeView } from './TreeView';
+export { Inspector } from './Inspector';
+export { Viewport } from './Viewport';
 export { TemplateMenu } from './TemplateMenu';
 export { SaucerEditor } from './SaucerEditor';
 export { useTeaAttrsContext } from './context/TeaAttrsContext';
