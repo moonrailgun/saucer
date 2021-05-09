d490064e597c543a009bf8d40544647beffa0160
moonrailgun <moonrailgun@gmail.com>
2021-05-09 22:18:57 +0800

    refactor: add custom cupNames with props

--- packages/editor/src/SaucerEditor.tsx
+++ packages/editor/src/SaucerEditor.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Inspector } from './Inspector';
-import { Previewer } from './Previewer';
 import { TemplateMenu } from './TemplateMenu';
 import { TreeView } from './TreeView';
 import { Viewport } from './Viewport';
@@ -10,11 +9,15 @@ import SplitPane from 'react-split-pane';
  * Default Saucer Editor
  * Also you can compose it by you self
  */
-export const SaucerEditor: React.FC = React.memo(() => {
+
+interface SaucerEditorProps {
+  cupNames?: string[];
+}
+export const SaucerEditor: React.FC<SaucerEditorProps> = React.memo((props) => {
   return (
     <>
       <SplitPane split="vertical" minSize={180}>
-        <TemplateMenu />
+        <TemplateMenu cupNames={props.cupNames} />
 
         <SplitPane split="vertical" primary="second" minSize={240}>
           <Viewport />
